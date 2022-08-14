fa550b107aeed5cb19330143c52a4c9348f34722
josegonzah <jose.gonzalezh@udea.edu.co>
2022-08-14 11:07:06 -0500

    Version estable. con readme

--- CoinCollector.py
+++ CoinCollector.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
-
 This is a temporary script file.
 """
 import cv2
 import numpy as np
 import random
 
+#Inicializo las variables con las que trabajaremos en el programa
 video = cv2.VideoCapture(0)
-
-kernel = np.ones((15, 15), np.uint8)
-
+kernel = np.ones((30, 30), np.uint8)
 backGround = cv2.imread('bg2.jpg')
 player = cv2.imread('mario.png')
 player = cv2.resize(player, (93, 80))
@@ -25,33 +23,34 @@ coinPositionXAxis= random.randint(1,backGroundWidth-coinWidth)
 score=0
 scoreAux=0
 loses = 0
-
+#Limpiamos las ventanas
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
+#Ciclo principal que procesa constatemente las imagenes y permite jugar el juego
 while True:
+    #Leo los frames y extraigo la informacion significativa para luego propecesarlo
     check,frame=video.read()
     frame=cv2.flip(frame,1)
     processedFrame = frame[:,:,0]
     processedFrame = cv2.subtract(processedFrame, cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY))
-    ret, processedFrame = cv2.threshold(processedFrame,100,255,cv2.THRESH_BINARY)
-    processedFrame = cv2.blur(processedFrame,(2,2))
+    ret, processedFrame = cv2.threshold(processedFrame,100,250,cv2.THRESH_BINARY)
+    processedFrame = cv2.blur(processedFrame,(4,4))
     processedFrame = cv2.dilate(processedFrame,kernel,iterations=1)
-    processedFrame = cv2.Canny(processedFrame,5,150)
+    processedFrame = cv2.Canny(processedFrame,100,250)
     contornos, cnts = cv2.findContours(processedFrame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     backGround = cv2.imread('bg2.jpg')
     if len(contornos)!=0:
         backGround[backGroundHeight-playerHeight:backGroundHeight, contornos[0][0][0][0]:contornos[0][0][0][0]+playerWidth] = player
-        if coinPositionYAxis+2>=backGroundHeight:
-            print(coinPositionYAxis)
-            coinPositionYAxis=coinHeight
-            coinPositionXAxis=random.randint(1,backGroundWidth-playerWidth)
-            loses=loses+1
-            backGround[coinPositionYAxis-coinHeight:coinPositionYAxis, coinPositionXAxis:coinPositionXAxis+coinWidth]=coin
-        if contornos[0][0][0][0]+playerWidth<=backGroundWidth and coinPositionYAxis<backGroundHeight-2:
+        if coinPositionYAxis+4>=backGroundHeight: #Checkeo si la moneda aun esta dentro del recuadro
+            coinPositionYAxis=coinHeight #Inicializo la poscion de la moneda en el inicio del fondo
+            coinPositionXAxis=random.randint(1,backGroundWidth-playerWidth)#Le damos un valor aleatorio en el eje X
+            loses=loses+1 #Sumamos una perdida
+            backGround[coinPositionYAxis-coinHeight:coinPositionYAxis, coinPositionXAxis:coinPositionXAxis+coinWidth]=coin #Actualizo la imagen de la moneda
+        if contornos[0][0][0][0]+playerWidth<=backGroundWidth and coinPositionYAxis<backGroundHeight-2: #Si la moneda aun esta en juego pinto
             backGround[backGroundHeight-playerHeight:backGroundHeight, contornos[0][0][0][0]:contornos[0][0][0][0]+playerWidth] = player
             backGround[coinPositionYAxis-coinHeight:coinPositionYAxis, coinPositionXAxis:coinPositionXAxis+coinWidth]=coin
             if coinPositionYAxis>=playerHeight and coinPositionXAxis>=contornos[0][0][0][0] and coinPositionXAxis<=contornos[0][0][0][0]+playerWidth:
+                #En caso de anotar elimino la moneda y sumo un punto
                 score +=1
                 scoreAux = score
                 if scoreAux > 0:
@@ -62,12 +61,14 @@ while True:
             cv2.putText(backGround, 'LOSES: '+str(loses), (10,30), cv2.FONT_HERSHEY_DUPLEX,1,(0,0,0),3)
         else:
             backGround = cv2.imread('bg2.jpg')
-        coinPositionYAxis+=2
+        coinPositionYAxis+=4
     else:
+        #El juego no inicia hasta que el programa no detecte el color azul
         backGround = cv2.imread('bg2.jpg')
         cv2.putText(backGround, 'COLOCA EL VASO AL FRENTE', (100, 200),cv2.FONT_HERSHEY_DUPLEX,1,(0,0,0),3)
         cv2.putText(backGround, 'PARA DETECTAR EL MANDO', (120, 250),cv2.FONT_HERSHEY_DUPLEX,1,(0,0,0),3)
-    if score==50 or loses==5:
+    if score==30 or loses==10:
+        #El juego termina cuando pierde 10 veces o agarra 30 monedas
         break
     cv2.imshow('COIN COLLECTOR',backGround)
     cv2.imshow('',frame)
@@ -79,10 +80,11 @@ while True:
 video.release()
 cv2.destroyAllWindows()
 
-if score==50:
+#Mensajes en caso de terminar el juego
+if score==30:
     cv2.putText(backGround, 'GANASTE', (250, 50), cv2.FONT_HERSHEY_DUPLEX,1,(0,0,0),3)
     (backGround, 'PRESIONA CUALQUIER TECLA PARA SALIR', (200, 450), cv2.FONT_HERSHEY_DUPLEX,1,(0,0,0),3)
-elif loses==5:
+elif loses==10:
     cv2.putText(backGround, 'PERDISTE', (250, 50), cv2.FONT_HERSHEY_DUPLEX,1,(0,0,0),3)
     (backGround, 'PRESIONA CUALQUIER TECLA PARA SALIR', (200, 450), cv2.FONT_HERSHEY_DUPLEX,1,(0,0,0),3)
 else:
---
+++ Readme.txt.txt
@@ -0,0 +1,9 @@
+-Descargue o clone el directorio de Git
+-Asegurese te tener las dependencias y librebrías actualizadas para el correcto funcionamiento
+-Para iniciar el juego asegurese de tener algún objeto azul que sea lo sucientemente grande (superficie visible a la cámara de aprox 5x5)
+El juego está pensado para vasos azules, así que no importa si su objeto es sólido o traslúcido, sólo importa el color y 
+el tamaño
+-Mueva el objeto para mover el jugador y atrapar las monedas
+-Una vez atrapadas 30 monedas o pérdidas 10 el juego se terminará
+-Para pausar el juego sólo basta con apartar el objeto de la cámara
+-Para salirse del juego presione la tecla Q
